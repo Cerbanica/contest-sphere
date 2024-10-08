cfae9730a2f2d6ce8d860d544a894f0e72ddd2b2
cerba <cerbadev@gmail.com>
2024-10-09 03:17:45 +0800

    added suspense 54

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes'; // Import ThemeProvider
-import {Navbar} from "./components";
+import { Navbar } from "./components";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,29 +19,32 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-   // Ensure the app renders the theme correctly during SSR
-   
+  // Ensure the app renders the theme correctly during SSR
+
   return (
     <html lang="en">
-       
-      <body className='relative '>
-      <ThemeProvider attribute="class">
-       <Navbar/>
-       <div className="flex flex-col min-h-screen pt-4 bg-gray-200 text-black dark:bg-gray-800 dark:text-white">
-     
-        <main className="flex-grow">
-          {children}
-        </main>
 
-      
-      
-      </div>
-      <footer className="flex flex-col w-full pt-4 text-center bg-white dark:bg-gray-800">
-          <span className="flex bg-transparent w-full order border-cs"></span>
-          <p className="bg-white py-4 dark:bg-gray-800">ContestSphere</p>
-        </footer>
-       </ThemeProvider>
-        </body>
+      <body className='relative '>
+        <ThemeProvider attribute="class">
+          <Navbar />
+          <div className="flex flex-col min-h-screen pt-4 bg-gray-200 text-black dark:bg-gray-800 dark:text-white">
+
+            <main className="flex-grow">
+              <Suspense fallback={<div>Loading...</div>}>
+
+                {children}
+              </Suspense>
+            </main>
+
+
+
+          </div>
+          <footer className="flex flex-col w-full pt-4 text-center bg-white dark:bg-gray-800">
+            <span className="flex bg-transparent w-full order border-cs"></span>
+            <p className="bg-white py-4 dark:bg-gray-800">ContestSphere</p>
+          </footer>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
